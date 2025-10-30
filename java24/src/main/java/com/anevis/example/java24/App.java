e41826f46d63acce6dbd009f7e095d6c96f51515
Jens Breitenstein <jens.breitenstein@anevis-solutions.com>
2025-10-30 16:23:50 +0100

    added two build agents (jdk8+mvn2 / jdk24+mvn3) added sample project (multiple Jenkinsfile's)

---
+++ java24/src/main/java/com/anevis/example/java24/App.java
@@ -0,0 +1,9 @@
+package com.anevis.example;
+
+public class App
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello from Java 24 + Maven 3!");
+    }
+}
---
+++ java8/src/main/java/com/anevis/example/java8/App.java
@@ -0,0 +1,9 @@
+package com.anevis.example;
+
+public class App
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello from Java 1.8 + Maven 2!");
+    }
+}
